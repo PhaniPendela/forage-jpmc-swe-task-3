251f08144deffe64eae2e603e9138aef63770981
PhaniPendela <67474140+PhaniPendela@users.noreply.github.com>
2024-05-29 18:43:45 +0530

    Part 3 Finished

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,37 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerResponse } from "http";
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  timestamp: Date;
+  price_abc: Number;
+  price_def: Number;
+  ratio: Number;
+  upper_bound: Number;
+  lower_bound: Number;
+  trigger_alert: Number | undefined;
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const priceABC =
+      (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF =
+      (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperbound = 1 + 0.05;
+    const lowerbound = 1 - 0.05;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio: ratio,
+      timestamp:
+        serverResponds[0].timestamp > serverResponds[1].timestamp
+          ? serverResponds[0].timestamp
+          : serverResponds[1].timestamp,
+      upper_bound: upperbound,
+      lower_bound: lowerbound,
+      trigger_alert:
+        ratio > upperbound || ratio < lowerbound ? ratio : undefined,
+    };
   }
 }
